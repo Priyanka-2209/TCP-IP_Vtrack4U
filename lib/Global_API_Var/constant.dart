add4e61380bec9952747d714d0f8e8200fea8bb7
Priyanka-2209 <priyanka.kapadia@absoluteweb.in>
2025-01-01 11:32:14 +0530

    Revert "first commit"
    This reverts commit 0958e895f143f8be0683749c2f0cca99af3b729f.
    
    # Conflicts:
    #	android/app/src/main/AndroidManifest.xml
    #	android/app/src/main/res/mipmap-hdpi/ic_launcher.png
    #	android/app/src/main/res/mipmap-mdpi/ic_launcher.png
    #	android/app/src/main/res/mipmap-xhdpi/ic_launcher.png
    #	android/app/src/main/res/mipmap-xxhdpi/ic_launcher.png
    #	android/app/src/main/res/mipmap-xxxhdpi/ic_launcher.png
    #	assets/icon/icon.png
    #	assets/images/splash_image.png
    #	ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-1024x1024@1x.png
    #	ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-20x20@1x.png
    #	ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-20x20@2x.png
    #	ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-20x20@3x.png
    #	ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-29x29@1x.png
    #	ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-29x29@2x.png
    #	ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-29x29@3x.png
    #	ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-40x40@1x.png
    #	ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-40x40@2x.png
    #	ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-40x40@3x.png
    #	ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-60x60@2x.png
    #	ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-60x60@3x.png
    #	ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-76x76@1x.png
    #	ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-76x76@2x.png
    #	ios/Runner/Assets.xcassets/AppIcon.appiconset/Icon-App-83.5x83.5@2x.png
    #	lib/custom_feild/CustomTextFeild.dart
    #	lib/main.dart
    #	lib/screens/login.dart
    #	lib/screens/tcp_connection.dart
    #	lib/services/logout_service.dart
    #	lib/services/user_service.dart
    #	lib/services/vehicle_service.dart
    #	linux/flutter/generated_plugin_registrant.cc
    #	linux/flutter/generated_plugins.cmake
    #	macos/Flutter/GeneratedPluginRegistrant.swift
    #	pubspec.lock
    #	pubspec.yaml
    #	windows/flutter/generated_plugin_registrant.cc
    #	windows/flutter/generated_plugins.cmake

--- lib/Global_API_Var/constant.dart
+++ lib/Global_API_Var/constant.dart
@@ -1,4 +1,3 @@
 class ApiConstants {
-  // static const String baseUrl = 'https://vtrack4u.co.in/tcpip/api/';
   static const String baseUrl = 'https://absolutewebservices.in/vtrack4utcpip/api/';
 }
